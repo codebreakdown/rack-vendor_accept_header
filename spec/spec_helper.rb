160eb9c28f0cf2a436641ccd94fa0003370c6143
Andrew De Ponte <cyphactor@gmail.com>
2015-11-07 02:25:51 -0800

    Add CodeClimate::TestReporter.start back
    Why you made the change:
    
    I did this because even though SimpleCov.start was generating a report
    locally. I don't think it was being pushed to code climate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'codeclimate-test-reporter'
 SimpleCov.start
+CodeClimate::TestReporter.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rack/vendor_accept_header'
