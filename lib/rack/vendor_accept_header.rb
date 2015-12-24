d5587c2c88c676980015507679677d93c001b65c
Andrew De Ponte <cyphactor@gmail.com>
2015-12-24 09:15:38 -0500

    Pulled regex out into constant
    Why you made the change:
    
    I did this because the line was greater than 80 and figured it was a
    nice way to reduce it's length. I also, moved the documentation blocks
    directly on top of their various components.

--- lib/rack/vendor_accept_header.rb
+++ lib/rack/vendor_accept_header.rb
@@ -1,18 +1,18 @@
-require "rack/vendor_accept_header/version"
-require "rack"
+require 'rack/vendor_accept_header/version'
+require 'rack'
 
 module Rack
   ##
   # This class is Rack middleware that handles parsing vendor HTTP
   # Accept headers
-
   class VendorAcceptHeader
+    VND_ACCEPT_REGEX=/application\/vnd\.(\w+)\.v(\d+(?:\.\d+)*)\+(\w+)/
+
     ##
     # Construct an instance of the Rack::VendorAcceptHeader middleware
     #
     # This takes the `app` as the first argument and a hash of `options`
     # as the second argument.
-
     def initialize(app, options = {})
       @app = app
       @options = options
@@ -26,7 +26,6 @@ module Rack
     # calling the `_call` method to perform the actual work. This is
     # done to make this rack middleware thread safe for instance
     # variable interactions within this rack middleware.
-
     def call(env)
       dup._call(env)
     end
@@ -37,7 +36,6 @@ module Rack
     # This method is where the business logic exists for parsing out the
     # components of the vendor HTTP Accept header and exposing them in
     # the application layer.
-
     def _call(env)
       parts = parse_accept_header(env)
       env['vnd_version'] = parts[:version]
@@ -50,7 +48,7 @@ module Rack
     private
 
     def parse_accept_header(env)
-      if env['HTTP_ACCEPT'] =~ /application\/vnd\.(\w+)\.v(\d+(?:\.\d+)*)\+(\w+)/
+      if env['HTTP_ACCEPT'] =~ VND_ACCEPT_REGEX
         return { type: 'application', context: $1, version: $2, sub_type: $3 }
       else
         return { type: nil, context: nil, version: nil, sub_type: nil }
