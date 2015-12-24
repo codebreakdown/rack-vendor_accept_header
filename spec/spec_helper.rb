8ee8142cca4e988881d98c3b286a238cfee768b4
Andrew De Ponte <cyphactor@gmail.com>
2015-12-24 08:35:21 -0500

    Manually configure CodeClimate Formatter
    Why you made the change:
    
    I did this because I was having problems with the
    CodeClimate::TestReporter.start method. So I figured I would try this
    way.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,13 @@
 require "simplecov"
+require "codeclimate-test-reporter"
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  CodeClimate::TestReporter::Formatter
+]
 SimpleCov.start do
   add_filter "/spec/"
 end
 
-puts "DREW: #{SimpleCov.root.inspect}"
-
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rack/vendor_accept_header'
 
