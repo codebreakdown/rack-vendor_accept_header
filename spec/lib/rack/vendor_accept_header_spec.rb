26514992a170a61ff947b7a5d7d67c693b5e733e
Andrew De Ponte <cyphactor@gmail.com>
2015-12-24 09:05:33 -0500

    Small formatting fixes identified by codeclimate
    Why you made the change:
    
    I did this to cleanup the code a bit and get a better grade on
    codeclimate of course.

--- spec/lib/rack/vendor_accept_header_spec.rb
+++ spec/lib/rack/vendor_accept_header_spec.rb
@@ -1,66 +1,66 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe "Rack::VendorAcceptHeader" do
-  let!(:app) {
+describe 'Rack::VendorAcceptHeader' do
+  let!(:app) do
     @app = Rack::Builder.new do
       use Rack::VendorAcceptHeader
-      run lambda { |env| [200, {}, []] }
+      run lambda { [200, {}, []] }
     end
-  }
+  end
 
-  it "has a version number" do
+  it 'has a version number' do
     expect(Rack::VendorAcceptHeader::VERSION).not_to be nil
   end
 
-  context "when not given a vendor mimetype accept header" do
-    it "vnd_version is nil" do
+  context 'when not given a vendor mimetype accept header' do
+    it 'vnd_version is nil' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/json'
       expect(last_request.env['vnd_version']).to be_nil
     end
 
-    it "vnd_context is nil" do
+    it 'vnd_context is nil' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/json'
       expect(last_request.env['vnd_context']).to be_nil
     end
 
-    it "vnd_type is nil" do
+    it 'vnd_type is nil' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/json'
       expect(last_request.env['vnd_type']).to be_nil
     end
 
-    it "vnd_sub_type is nil" do
+    it 'vnd_sub_type is nil' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/json'
       expect(last_request.env['vnd_sub_type']).to be_nil
     end
   end
 
-  context "when given a valid vendor mimetype accept header" do
-    it "extracts the context" do
+  context 'when given a valid vendor mimetype accept header' do
+    it 'extracts the context' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/vnd.acme.v1+json'
       expect(last_request.env['vnd_context']).to eq('acme')
     end
 
-    it "extracts single digit versions" do
+    it 'extracts single digit versions' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/vnd.acme.v5+json'
       expect(last_request.env['vnd_version']).to eq('5')
     end
 
-    it "extracts double digit versions" do
+    it 'extracts double digit versions' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/vnd.acme.v1.2+json'
       expect(last_request.env['vnd_version']).to eq('1.2')
     end
 
-    it "extracts tripple digit versions" do
+    it 'extracts tripple digit versions' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/vnd.acme.v1.2.3+json'
       expect(last_request.env['vnd_version']).to eq('1.2.3')
     end
 
-    it "extracts the type" do
+    it 'extracts the type' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/vnd.acme.v2+json'
       expect(last_request.env['vnd_type']).to eq('application')
     end
 
-    it "extracts the sub type" do
+    it 'extracts the sub type' do
       get '/', {}, 'HTTP_ACCEPT' => 'application/vnd.acme.v2+json'
       expect(last_request.env['vnd_sub_type']).to eq('json')
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
-require "simplecov"
-require "codeclimate-test-reporter"
+require 'simplecov'
+require 'codeclimate-test-reporter'
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   CodeClimate::TestReporter::Formatter
 ]
 SimpleCov.start do
-  add_filter "/spec/"
+  add_filter '/spec/'
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
