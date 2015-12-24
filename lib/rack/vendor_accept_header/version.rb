f540318b1c704d5eb4b9fbf29b4e66a0f8693030
Andrew De Ponte <cyphactor@gmail.com>
2015-12-24 09:40:45 -0500

    Switch to new lambda syntax
    Why you made the change:
    
    I did this because it is the new literal lamda syntax. I also, changed
    the argument from env to _env to indicate that it is garbage in this
    particular case.

--- lib/rack/vendor_accept_header/version.rb
+++ lib/rack/vendor_accept_header/version.rb
@@ -1,5 +1,5 @@
 module Rack
   class VendorAcceptHeader
-    VERSION = "1.0.0"
+    VERSION = '1.0.0'
   end
 end
--- spec/lib/rack/vendor_accept_header_spec.rb
+++ spec/lib/rack/vendor_accept_header_spec.rb
@@ -4,7 +4,7 @@ describe 'Rack::VendorAcceptHeader' do
   let!(:app) do
     @app = Rack::Builder.new do
       use Rack::VendorAcceptHeader
-      run lambda { |env| [200, {}, []] }
+      run ->(_env) { [200, {}, []] }
     end
   end
 
